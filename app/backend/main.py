9d749c0e2524f65cbaf8b9c502f42fe56cf4806d
aleffericlys <ericlys558@gmail.com>
2024-03-24 20:10:46 -0300

    otimizando o backend

--- app/backend/main.py
+++ app/backend/main.py
@@ -6,7 +6,10 @@ App = Flask('transport')
 App.config.from_object(__name__)
 
 CORS(App)
-with open('app/backend/data.json', 'r') as file:
+
+# with open('app/backend/data.json', 'r') as file:
+
+with open('data.json', 'r') as file:
 	data = json.load(file)
 
 data = data['transport']
@@ -15,17 +18,57 @@ data = data['transport']
 def transport():
 	return data
 
-@App.route('/transport/<city>', methods=['GET'])
-def transport_id(city):
+
+def find_city(city):
 	result = []
 	for item in data:
 		if item['city'] == city:
 			result.append(item)
 
+	return result
+
+
+
+def find_cheapest(city):
+	routes = find_city(city)
+	if len(routes) == 0:
+		return []
+	else:
+		cheapest = routes[0]
+		for route in routes:
+			if float(route['price_econ'].replace("R$ ", '')) < float(cheapest['price_econ'].replace("R$ ", '')):
+				cheapest = route
+		return cheapest
+
+def find_fastest(city):
+	routes = find_city(city)
+	if len(routes) == 0:
+		return []
+	else:
+		fastest = routes[0]
+		for route in routes:
+			if int(route['duration'].replace('h', '')) < int(fastest['duration'].replace('h', '')):
+				fastest = route 
+		return fastest
+
+@App.route('/transport/<city>', methods=['GET'])
+def transport_id(city):
+	result = []
+	fastest = find_fastest(city)
+	cheapest = find_cheapest(city)
+
+	if fastest != []:
+		result.append(fastest) 
+	
+	if cheapest != []:
+		if cheapest != fastest:
+			result.append(cheapest)
+	
 	if result == []:
 		return 'Not found', 404
-	
-	return jsonify(result)
+	else:
+		return jsonify(result)
+
 
 @App.route('/transport_cityes', methods=['GET'])
 def transport_city():
@@ -38,3 +81,5 @@ def transport_city():
 
 if __name__ == '__main__':
 	App.run(debug=True, port=3000)
+
+
