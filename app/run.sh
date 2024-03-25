6c57fb5ee9a61b557215522e08fc68b85629e6ff
aleffericlys <ericlys558@gmail.com>
2024-03-24 23:24:32 -0300

    mais um teste com o run.sh

--- app/run.sh
+++ app/run.sh
@@ -2,6 +2,5 @@
 
 pip install flask
 pip install flask-cors
-npm install vue
 
-python3 app/backend/main.py & cd app/frontend && npm run serve
+python3 app/backend/main.py & cd app/frontend && npm install && npm run serve
