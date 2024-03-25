89e6d03d7a80f8663260499eb3a2d805bcac7782
aleffericlys <ericlys558@gmail.com>
2024-03-24 23:04:27 -0300

    alterações pra testes

--- app/frontend/vue.config.js
+++ app/frontend/vue.config.js
@@ -1,12 +1,15 @@
 const webpack = require('webpack');
 
 module.exports = {
-  transpileDependencies: true,
-  configureWebpack: {
-    plugins: [
-      new webpack.DefinePlugin({
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: true
-      })
-    ]
+	transpileDependencies: [
+	  'vue-echarts',
+	  'resize-detector'
+	],
+	configureWebpack: {
+	  plugins: [
+		new webpack.DefinePlugin({
+		  __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: true
+		})
+	  ]
+	}
   }
-}
